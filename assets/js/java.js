ea73134b42aef9d5fc5c46cf1abb9cf7ef5e5356
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2023-08-28 11:46:22 +0200

    alle 4 opgaver er løst

---
+++ assets/js/java.js
@@ -0,0 +1,35 @@
+console.group('Opgave 1');
+const navn = 'Tanja Hedemann Nielsen';
+console.log(navn);
+console.groupEnd('Opgave 1');
+
+console.group('opgave 2');
+const tal1 = 5;
+const tal2 = 6;
+let sum = tal1+tal2;
+console.log(sum);
+console.groupEnd('opgave 2');
+
+console.group('opgave 3');
+let alder = 36;
+alder = 41;
+console.log(alder);
+console.groupEnd('opgave 3');
+
+console.group('opgave 4');
+let myNumber = 1;
+
+myNumber = myNumber + 1; // Inkrementér
+console.log(myNumber);
+
+myNumber = myNumber - 1; // Dekrementér
+console.log(myNumber);
+//ALTERNATIV
+let myNumber2 = 1;
+
+myNumber2++; // Inkrementér
+console.log(myNumber2);
+
+myNumber2--; // Dekrementér
+console.log(myNumber2);
+console.groupEnd('opgave 4');
